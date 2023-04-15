7103677258ffbcc252b2de7d7f930deeee736d72
Muhammad Junaid <106451888+Muhammad-Junaid95@users.noreply.github.com>
2023-04-15 16:42:46 +0500

    Update classQuizz.dart

--- classQuizz.dart
+++ classQuizz.dart
@@ -1,1 +1,2 @@
-
+////the sequence 11222333377777 should be transmitted as 21324357
+//Write a program for the probe to perform this encoding. The program will take the original number as long integer, string or array of numbers and return the frequency-digit sequence as a long integer, string or array of numbers.
