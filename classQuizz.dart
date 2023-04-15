e3631b5969d3c95b2f2922a32dd4805e0714561b
Muhammad Junaid <106451888+Muhammad-Junaid95@users.noreply.github.com>
2023-04-15 17:27:03 +0500

    Update classQuizz.dart

--- classQuizz.dart
+++ classQuizz.dart
@@ -1,2 +1,9 @@
 ////the sequence 11222333377777 should be transmitted as 21324357
 //Write a program for the probe to perform this encoding. The program will take the original number as long integer, string or array of numbers and return the frequency-digit sequence as a long integer, string or array of numbers.
+
+void main()
+{
+	var input = int.parse("12311122554");
+	printRLE(input);
+	return;
+}
